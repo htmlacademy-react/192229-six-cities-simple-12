e38107304216b58fca6d64aafa3054e690f813be
Sergey Z <zamsergey@gmail.com>
2023-03-25 17:29:37 +0300

    test connect

--- project/src/pages/property/property.tsx
+++ project/src/pages/property/property.tsx
@@ -97,7 +97,6 @@ function Property(prop: RoomOffers) : JSX.Element {
             </div>
             <section className="property__reviews reviews">
               <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">{reviews.length}</span></h2>
-
               <ReviewsList reviews={reviews}/>
               <ReviewsForm />
             </section>
